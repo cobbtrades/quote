08557b9d074533ef147b15ee0abe03f89bf846ef
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-20 08:34:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import ParagraphStyle
 from reportlab.lib import colors
 import pandas as pd
 
