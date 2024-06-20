1e0f9da94656d828b4489d098eae2eb835c817f1
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-20 14:00:22 -0400

    Update home.py

--- home.py
+++ home.py
@@ -87,7 +87,7 @@ def generate_pdf(data, filename='quote.pdf'):
     breakdown_table.setStyle(TableStyle([
         ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
         ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('ALIGN', (0, 0), (-1, -1), 'RIGHT'),
         ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
         ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
         ('BACKGROUND', (0, 1), (-1, -1), colors.whitesmoke),
