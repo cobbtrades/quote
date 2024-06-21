7d7c242d918755de916452f2ee6e33d323ceacea
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-21 13:37:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def generate_pdf(data, filename='quote.pdf'):
         ["CITY", data['city'], "STATE", data['state']],
         ["ZIP", data['zip'], "PHONE", data['cell_phone']]
     ]
-    details_table = Table(details_data, colWidths=[80, 180, 60, 150])
+    details_table = Table(details_data, colWidths=[80, 150, 60, 180])
     details_table.setStyle(TableStyle([
         ('GRID', (0, 0), (-1, -1), 1, colors.black),
         ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
