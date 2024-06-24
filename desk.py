e022c8be11ba317e291a6be5e0d09bf7b19fadb7
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-24 13:21:12 -0400

    Update desk.py

--- desk.py
+++ desk.py
@@ -268,10 +268,6 @@ def render_tab(calc_payment_func, prefix, is_lease=False):
         dealer = st.text_input(label="Dealership", key=f"{prefix}_dealer", placeholder="Dealership", label_visibility="collapsed", help="Dealership")
         consultant = st.text_input(label="Sales Person", key=f"{prefix}_consultant", placeholder="Sales Person", label_visibility="collapsed", help="Sales Person")
         manager = st.text_input(label="Sales Manager", key=f"{prefix}_manager", placeholder="Sales Manager", label_visibility="collapsed", help="Sales Manager")
-        if is_lease:
-            l_left, l_right = st.columns(2)
-            l_left.markdown('<input class="label-input" type="text" value="Residual" disabled>', unsafe_allow_html=True)
-            residual_value = l_right.number_input(label="Residual Value", key=f"{prefix}_residual_value", value=0, label_visibility="collapsed")
     
     left_col, right_col = st.columns(2)
     
@@ -366,6 +362,10 @@ def render_tab(calc_payment_func, prefix, is_lease=False):
         col3.markdown(f'<div class="centered-metric"><div class="stMetric"><span style="font-size: 14px;">{ltv1:.2f}%</span></div></div>', unsafe_allow_html=True)
         col4.markdown(f'<div class="centered-metric"><div class="stMetric"><span style="font-size: 14px;">{ltv2:.2f}%</span></div></div>', unsafe_allow_html=True)
         col5.markdown(f'<div class="centered-metric"><div class="stMetric"><span style="font-size: 14px;">{ltv3:.2f}%</span></div></div>', unsafe_allow_html=True)
+
+        if is_lease:
+                col1.markdown('<input class="label-input" type="text" value="Residual" disabled>', unsafe_allow_html=True)
+                residual_value = col2.number_input(label="Residual Percent", key=f"{prefix}_residual_percent", value=0, label_visibility="collapsed")
     
     lbc, rbc, blankbc = st.columns([2, 3, 10])
     with lbc:
