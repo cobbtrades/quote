f6acb63e7644a4d0cf627ae808442ddbd7718271
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-24 09:54:45 -0400

    Update desk.py

--- desk.py
+++ desk.py
@@ -266,7 +266,7 @@ with finance:
         labels_col, inputs_col = st.columns([1, 3])
     
         def centered_label(text):
-            return f'<div style="display: flex; align-items: center; height: 100%; top-padding: 10px;">{text}</div>'
+            return f'<div style="display: flex; align-items: center; height: 100%; top-padding: 15px;">{text}</div>'
     
         labels_col.markdown(centered_label("Market Value"), unsafe_allow_html=True)
         market_value = inputs_col.number_input(label="Market Value", key="market_value", value=None, placeholder="Market Value", label_visibility='collapsed', help="Market Value")
