10a1d2a4af1124eb54af3037104f49efa4355757
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-24 13:49:26 -0400

    Update desk.py

--- desk.py
+++ desk.py
@@ -10,8 +10,6 @@ st.set_page_config(page_title="Desking App")
 with open("styles.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-st.header("Desking App")
-
 def calculate_monthly_payment(principal, down_payment, annual_rate, term_months):
     if principal == 0:
         return 0
