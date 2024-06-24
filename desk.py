0ffad1cb85b351cde50f27e3a6bfe71a7890180e
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-24 11:35:19 -0400

    Update desk.py

--- desk.py
+++ desk.py
@@ -396,6 +396,6 @@ with lbc:
         with open(pdf_file, 'rb') as f:
             st.download_button('Download PDF Quote', f, file_name=pdf_file)
     with rbc:
-        gross_profit = market_value - discount - cost_of_vehicle + (acv_of_trade - trade_value)
+        gross_profit = market_value - discount - veh_cost + (trade_acv - trade_value)
         color = "lightgreen" if gross_profit > 0 else "red" if gross_profit < 0 else "white"
         st.markdown(f"<p style='color:{color}; font-size:24px;'>Front Gross ${gross_profit:.2f}</p>", unsafe_allow_html=True)
