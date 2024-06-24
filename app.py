2242b9687c104c55e57c31e57a8ca447a20b8b45
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-24 19:40:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ SALES_TAX_RATE_NC = 0.03
 
 # Function to calculate monthly payments
 def calculate_monthly_payment(principal, annual_rate, term_months):
-    monthly_rate = annual_rate / 12
+    monthly_rate = annual_rate /100 / 12
     if monthly_rate == 0:
         return principal / term_months
     return principal * monthly_rate / (1 - (1 + monthly_rate) ** -term_months)
