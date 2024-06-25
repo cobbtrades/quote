f5d963ea7165c1de0dfaad6efbca70a200d0e543
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-25 16:10:25 -0400

    Update desk.py

--- desk.py
+++ desk.py
@@ -308,10 +308,18 @@ def render_tab(calc_payment_func, prefix, is_lease=False):
         year = tc4.text_input(label="Year", key=f"{prefix}_year", max_chars=4, label_visibility="collapsed", help="Year")
         fr4.markdown('<input class="label-input" type="text" value="Make" disabled>', unsafe_allow_html=True)
         make = ft4.text_input(label="Make", key=f"{prefix}_make", label_visibility="collapsed", help="Make")
-        fc5, sc5, tc5 = st.columns([3, 1.5, 1.5])
-        model = fc5.text_input(label="Model", key=f"{prefix}_model", placeholder="Model", label_visibility="collapsed", help="Model")
-        trim = sc5.text_input(label="Trim", key=f"{prefix}_trim", max_chars=4, placeholder="Trim", label_visibility="collapsed", help="Trim")
-        odometer = tc5.text_input(label="Odometer", key=f"{prefix}_odometer", placeholder="Odometer", label_visibility="collapsed", help="Odometer")
+        
+        
+        fc5, sc5, tc5, fr5, ft5, st5 = st.columns([1, 3, 1, 1.5, 1, 1.5])
+        fc5.markdown('<input class="label-input" type="text" value="Model" disabled>', unsafe_allow_html=True)
+        model = sc5.text_input(label="Model", key=f"{prefix}_model", placeholder="Model", label_visibility="collapsed", help="Model")
+        tc5.markdown('<input class="label-input" type="text" value="Trim" disabled>', unsafe_allow_html=True)
+        trim = fr5.text_input(label="Trim", key=f"{prefix}_trim", max_chars=4, placeholder="Trim", label_visibility="collapsed", help="Trim")
+        ft5.markdown('<input class="label-input" type="text" value="Odometer" disabled>', unsafe_allow_html=True)
+        odometer = st5.text_input(label="Odometer", key=f"{prefix}_odometer", placeholder="Odometer", label_visibility="collapsed", help="Odometer")
+
+
+        
         fc6, sc6, tc6, fr6 = st.columns(4)
         fc6.markdown('<input class="label-input" type="text" value="Cost" disabled>', unsafe_allow_html=True)
         veh_cost = sc6.number_input(label="Cost", key=f"{prefix}_veh_cost", value=0, label_visibility='collapsed', help="Cost")
