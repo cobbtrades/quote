d32f3163294612e4c3856d5f9cca0c5d17ae39dc
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-07-05 18:40:01 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -133,9 +133,7 @@ def fill_fi_pdf(in_path, out_path, data):
     pdfrw.PdfWriter().write(out_path, pdf)
 
 def modify_stocknum(stocknum):
-    if stocknum is None:
-        return ''
-    elif stocknum[-1].isdigit():
+    if stocknum[-1].isdigit():
         return stocknum + 'A'
     else:
         last_char = stocknum[-1]
