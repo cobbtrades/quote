f32f9e7f0cf8a63f2bb4cda7fc2b02d2a3ea8207
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-07-12 16:35:02 -0400

    Update home.py

--- home.py
+++ home.py
@@ -266,7 +266,7 @@ def render_tab(calc_payment_func, prefix, is_lease=False):
                 quotes[terms[i]] = term_payments
             final_monthly_payment = calc_payment_func(balance, down_payments[0], rates[0], terms[0])
             final_total_amount_paid = monthly_payment * terms[0]
-            lawfinancecharge = final_total_amount_paid - (balance - down_payments[0])
+            #lawfinancecharge = final_total_amount_paid - (balance - down_payments[0])
             data = {
                 "bos_date": datetime.today().strftime('%m/%d/%Y'),
                 "bos_salesperson": consultant,
@@ -576,7 +576,7 @@ def render_tab(calc_payment_func, prefix, is_lease=False):
                 "LAWMAKEMODEL": f"{make} {model}",
                 "LAWVIN": vin,
                 "LAWRATE": f"{rates[0]:.2f}",
-                "LAWFINANCECHARGE": "{:.2f}".format(lawfinancecharge),
+                "LAWFINANCECHARGE": '',
                 "LAWAMTFINANCED": '',
                 "LAWTOTALPAY": '',
                 "LAWDOWNPAY": "{:.2f}".format(value1),
